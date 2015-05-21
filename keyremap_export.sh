339945b6ae32cd31212719bec6154509c726a3de
Alex Long <alex.long@invincea.com>
2015-05-21 17:39:11 -0400

    Add KeyRemap4MacBook config

---
+++ keyremap_export.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set remap.commandDelete2optionLdelete 1
+/bin/echo -n .
+$cli set private.left_ctrl_to_left_option 1
+/bin/echo -n .
+$cli set private.controll_and_del_to_optionl_and_del 1
+/bin/echo -n .
+$cli set remap.swap_minus_and_underscore 1
+/bin/echo -n .
+$cli set remap.optionrcommandr 1
+/bin/echo -n .
+$cli set remap.controlR2commandR 1
+/bin/echo -n .
+$cli set remap.pclikehomeend 1
+/bin/echo -n .
+$cli set repeat.wait 43
+/bin/echo -n .
+$cli set option.vimode_fn_hjkl_rapid 1
+/bin/echo -n .
+$cli set remap.optionL2commandL 1
+/bin/echo -n .
+$cli set remap.commandL2optionL 1
+/bin/echo -n .
+/bin/echo
